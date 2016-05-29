bffc757cc043aa439d606b402036001d82e168e0
prad9 <vishnu.aman@outlook.com>
2016-05-30 00:27:21 +1200

    Wrote integration tests to all controller methods, add equality methods to product option to ignore IsNew when asserting

--- refactor-me.IntegrationTests/ProductsControllerTests.cs
+++ refactor-me.IntegrationTests/ProductsControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -19,10 +20,12 @@ namespace refactor_me.IntegrationTests
         {
             _productsController = new ProductsController();
             _productIds = new List<Guid>();
+            _productOptionIds = new List<Guid>();
         }
 
         private ProductsController _productsController;
         private List<Guid> _productIds;
+        private List<Guid> _productOptionIds;
 
         [TearDown]
         public void TearDown()
@@ -31,13 +34,67 @@ namespace refactor_me.IntegrationTests
             {
                 _productsController.Delete(productId);
             }
+            foreach (var productOptionId in _productOptionIds)
+            {
+                _productsController.DeleteOption(productOptionId);
+            }
         }
 
         [Test]
-        public void Create()
+        public void Create_And_GetProduct()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var expected = new Product(productId)
+            {
+                Id = productId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                Price = 123.45M,
+                DeliveryPrice = 67.89M
+            };
+            _productsController.Create(expected);
+
+            _productIds.Add(productId);
+
+            //Act
+            var actual = _productsController.GetProduct(productId);
+            
+            //Assert
+            actual.Should().Be(expected);
+        }
+
+        [Test]
+        public void Delete()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var product = new Product(productId)
+            {
+                Id = productId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                Price = 123.45M,
+                DeliveryPrice = 67.89M
+            };
+            _productsController.Create(product);
+
+            _productIds.Add(productId);
+
+            //Act
+            _productsController.Delete(productId);
+
+            //Assert
+            _productsController.Invoking(controller => controller.GetProduct(productId))
+                .ShouldThrow<HttpResponseException>();
+        }
+
+        [Test]
+        public void GetAll()
         {
             //Arrange
             var productId1 = Guid.NewGuid();
+            var prouctId2 = Guid.NewGuid();
             var product1 = new Product(productId1)
             {
                 Id = productId1,
@@ -46,150 +103,193 @@ namespace refactor_me.IntegrationTests
                 Price = 123.45M,
                 DeliveryPrice = 67.89M
             };
+            var product2 = new Product(prouctId2)
+            {
+                Id = prouctId2,
+                Name = "Dummy Name 2",
+                Description = "Dummy Description 2",
+                Price = 543.21M,
+                DeliveryPrice = 98.76M
+            };
             _productsController.Create(product1);
+            _productsController.Create(product2);
 
             _productIds.Add(productId1);
-
-            //Act
-            var actual = _productsController.GetProduct(productId1);
+            _productIds.Add(prouctId2);
             
+            //Act
+            var actual = _productsController.GetAll();
+
             //Assert
-            actual.Should().Be(product1);
+            actual.Items.Should().Contain(product1);
+            actual.Items.Should().Contain(product2);
         }
 
-        //[Test]
-        //public void GetAll()
-        //{
-        //    //Arrange
-        //    var productId1 = new Guid();
-        //    var prouctId2 = new Guid();
-        //    var product1 = new Product(productId1)
-        //    {
-        //        Id = productId1,
-        //        Name = "Dummy Name 1",
-        //        Description = "Dummy Description 1",
-        //        Price = 123.45M,
-        //        DeliveryPrice = 67.89M
-        //    };
-        //    var product2 = new Product(prouctId2)
-        //    {
-        //        Id = prouctId2,
-        //        Name = "Dummy Name 2",
-        //        Description = "Dummy Description 2",
-        //        Price = 543.21M,
-        //        DeliveryPrice = 98.76M
-        //    };
-        //    _productsController.Create(product1);
-        //    _productsController.Create(product2);
+        [Test]
+        public void SearchByName()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var expected = new Product(productId)
+            {
+                Id = productId,
+                Name = productId.ToString(),
+                Description = "Dummy Description 1",
+                Price = 123.45M,
+                DeliveryPrice = 67.89M
+            };
+            _productsController.Create(expected);
 
-        //    _productIds.Add(productId1);
-        //    _productIds.Add(prouctId2);
+            _productIds.Add(productId);
 
-        //    //Act
-        //    var products = _productsController.GetAll();
+            //Act
+            var actual = _productsController.SearchByName(productId.ToString());
 
-        //    //Assert
-        //    products.Items.Should().Contain(product1);
-        //    products.Items.Should().Contain(product2);
-        //}
+            //Assert
+            actual.Items.Should().Contain(expected);
+        }
 
-        //[Test]
-        //public void SearchByName()
-        //{
-        //    //Arrange
+        [Test]
+        public void Update()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var product = new Product(productId)
+            {
+                Id = productId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                Price = 123.45M,
+                DeliveryPrice = 67.89M
+            };
+            _productsController.Create(product);
 
-        //    //Act
+            var expected = product;
+            expected.Name = "Dummy Name 1 updated";
 
-        //    //Assert
+            _productIds.Add(productId);
 
-        //}
+            //Act
+            _productsController.Update(productId,expected);
+            var actual = _productsController.GetProduct(productId);
 
-        //[Test]
-        //public void GetProduct()
-        //{
-        //    //Arrange
+            //Assert
+            actual.Should().Be(expected);
+        }
 
-        //    //Act
+        [Test]
+        public void CreateOption_GetOption()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var optionId = Guid.NewGuid();
+            var expected = new ProductOption(optionId)
+            {
+                Id = optionId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                ProductId = productId
+            };
+            _productsController.CreateOption(productId,expected);
 
-        //    //Assert
+            _productOptionIds.Add(optionId);
+            
+            //Act
+            var actual = _productsController.GetOption(productId,optionId);
 
-        //}
+            //Assert
+            actual.Should().Be(expected);
+        }
 
-        //[Test]
-        //public void Update()
-        //{
-        //    //Arrange
+        [Test]
+        public void DeleteOption()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var optionId = Guid.NewGuid();
+            var expected = new ProductOption(optionId)
+            {
+                Id = optionId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                ProductId = productId
+            };
+            _productsController.CreateOption(productId, expected);
+            
+            //Act
+            _productsController.DeleteOption(optionId);
+           
+            //Assert
+            //Assert
+            _productsController.Invoking(controller => controller.GetOption(productId, optionId))
+                .ShouldThrow<HttpResponseException>();
+        }
 
-        //    //Act
+        [Test]
+        public void GetOptions()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
 
-        //    //Assert
+            var optionId1 = Guid.NewGuid();
+            var option1 = new ProductOption(optionId1)
+            {
+                Id = optionId1,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                ProductId = productId
+            };
 
-        //}
+            var optionId2 = Guid.NewGuid();
+            var option2 = new ProductOption(optionId1)
+            {
+                Id = optionId2,
+                Name = "Dummy Name 2",
+                Description = "Dummy Description 2",
+                ProductId = productId
+            };
 
-        //[Test]
-        //public void Delete()
-        //{
-        //    //Arrange
+            _productsController.CreateOption(productId, option1);
+            _productsController.CreateOption(productId, option2);
 
-        //    //Act
+            _productOptionIds.Add(optionId1);
+            _productOptionIds.Add(optionId2);
 
-        //    //Assert
+            //Act
+            var actual = _productsController.GetOptions(productId);
 
-        //}
+            //Assert
+            actual.Items.Should().Contain(option1);
+            actual.Items.Should().Contain(option2);
+        }
 
-        //[Test]
-        //public void GetOptions()
-        //{
-        //    //Arrange
+        [Test]
+        public void UpdateOption()
+        {
+            //Arrange
+            var productId = Guid.NewGuid();
+            var optionId = Guid.NewGuid();
+            var option = new ProductOption(optionId)
+            {
+                Id = optionId,
+                Name = "Dummy Name 1",
+                Description = "Dummy Description 1",
+                ProductId = productId
+            };
+            _productsController.CreateOption(productId, option);
 
-        //    //Act
+            _productOptionIds.Add(optionId);
 
-        //    //Assert
+            var expected = option;
+            expected.Name = "Dummy Name 1 updated";
 
-        //}
+            //Act
+            _productsController.UpdateOption(optionId,expected);
+            var actual = _productsController.GetOption(productId, optionId);
 
-        //[Test]
-        //public void GetOption()
-        //{
-        //    //Arrange
+            //Assert
+            actual.Should().Be(option);
+        }
 
-        //    //Act
-
-        //    //Assert
-
-        //}
-
-        //[Test]
-        //public void CreateOption()
-        //{
-        //    //Arrange
-
-        //    //Act
-
-        //    //Assert
-
-        //}
-
-        //[Test]
-        //public void UpdateOption()
-        //{
-        //    //Arrange
-
-        //    //Act
-
-        //    //Assert
-
-        //}
-
-        //[Test]
-        //public void DeleteOption()
-        //{
-        //    //Arrange
-
-        //    //Act
-
-        //    //Assert
-
-        //}
     }
 }
--- refactor-me/Models/Products.cs
+++ refactor-me/Models/Products.cs
@@ -164,6 +164,31 @@ namespace refactor_me.Models
 
         public string Description { get; set; }
 
+        protected bool Equals(ProductOption other)
+        {
+            return Id.Equals(other.Id) && ProductId.Equals(other.ProductId) && string.Equals(Name, other.Name) && string.Equals(Description, other.Description);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ProductOption) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id.GetHashCode();
+                hashCode = (hashCode*397) ^ ProductId.GetHashCode();
+                hashCode = (hashCode*397) ^ (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (Description != null ? Description.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
         [JsonIgnore]
         public bool IsNew { get; }
 
